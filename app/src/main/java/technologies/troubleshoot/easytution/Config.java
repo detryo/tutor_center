de30d5a4fb6d35e9cf9f7a62f0cefd4d78ce36b4
xubi <shoumikxbox@gmail.com>
2017-01-29 17:31:39 +0600

    fresh

--- app/src/main/java/technologies/troubleshoot/easytution/Config.java
+++ app/src/main/java/technologies/troubleshoot/easytution/Config.java
@@ -4,8 +4,6 @@ package technologies.troubleshoot.easytution;
  * Created by xubi and kaizer on 30-Dec-16.
  */
 
-//test
-
 public class Config {
     public static final String DATA_URL = "http://tuition.troubleshoot-tech.com/user.php?email=";
     public static final String KEY_NAME = "name";
