f95a3b2d56bee8eaaa39d992ee72a27c559a6fc3
kaizer <mir.rayan1@gmail.com>
2017-02-02 16:47:11 +0600

    trying to send post to the server

--- app/src/main/java/technologies/troubleshoot/easytution/DashBoard.java
+++ app/src/main/java/technologies/troubleshoot/easytution/DashBoard.java
@@ -17,11 +17,8 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.NetworkError;
-import com.android.volley.NoConnectionError;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
@@ -31,6 +28,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import static technologies.troubleshoot.easytution.LoginActivity.KEY_EMAIL;
 
+
 public class DashBoard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
@@ -39,8 +37,8 @@ public class DashBoard extends AppCompatActivity implements NavigationView.OnNav
         //getUserName();
         String email;
 
-        //Default fragment to be called on app start.
-        getSupportFragmentManager().beginTransaction().add(R.id.content_news_feed, new JobFeedFragment())
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.content_news_feed, new JobFeedFragment())
                 .commit();
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
@@ -135,7 +133,7 @@ public class DashBoard extends AppCompatActivity implements NavigationView.OnNav
         } else if (id == R.id.nav_new_post_id) {
 
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.content_news_feed, new StudentPostFragment())
+                    .replace(R.id.content_news_feed, new JobPostFragment())
                     .commit();
 
         } else if (id == R.id.nav_settings_id) {
@@ -184,18 +182,6 @@ public class DashBoard extends AppCompatActivity implements NavigationView.OnNav
                     new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
-                            String message = null;
-                            if ( error instanceof NetworkError) {
-                                message = "Cannot connect to Internet...Please check your connection!";
-                                Toast.makeText(DashBoard.this, message, Toast.LENGTH_LONG).show();
-                            }
-                            else if (error instanceof NoConnectionError) {
-                                message = "Cannot connect to Internet...Please check your connection!";
-                                Toast.makeText(DashBoard.this, message, Toast.LENGTH_LONG).show();
-                            } else if (error instanceof TimeoutError) {
-                                message = "Connection TimeOut! Please check your internet connection.";
-                                Toast.makeText(DashBoard.this, message, Toast.LENGTH_LONG).show();
-                            }
                             Toast.makeText(DashBoard.this, "Stop using this app", Toast.LENGTH_LONG).show();
                         }
                     });
