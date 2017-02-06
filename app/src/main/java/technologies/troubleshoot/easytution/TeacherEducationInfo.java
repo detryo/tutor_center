f52163e9a97c9353ad6c750afe35153f6a41acc0
kaizer <mir.rayan1@gmail.com>
2017-02-06 18:25:26 +0600

    teacher profile info all editText taged

--- app/src/main/java/technologies/troubleshoot/easytution/TeacherEducationInfo.java
+++ app/src/main/java/technologies/troubleshoot/easytution/TeacherEducationInfo.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by kaizer on 2/6/17.
@@ -13,10 +14,21 @@ import android.view.ViewGroup;
 
 public class TeacherEducationInfo extends Fragment {
 
+    EditText lastLevelOfStudyEditText, majorEditText, instituteNameEditText, cgpaEditText, yearOfpassingEditText, curriculumEditText, fromEditText, toEditText;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.teacher_educational_info_layout, container, false);
+
+        lastLevelOfStudyEditText = (EditText) rootView.findViewById(R.id.last_level_of_study_edit_view_id);
+        majorEditText = (EditText) rootView.findViewById(R.id.major_group_edit_view_id);
+        instituteNameEditText = (EditText) rootView.findViewById(R.id.institute_name_edit_view_id);
+        cgpaEditText = (EditText) rootView.findViewById(R.id.cgpa_edit_view_id);
+        yearOfpassingEditText = (EditText) rootView.findViewById(R.id.years_of_passing_edit_view_id);
+        curriculumEditText = (EditText) rootView.findViewById(R.id.curriculum_edit_view_id);
+        fromEditText = (EditText) rootView.findViewById(R.id.from_edit_view_id);
+        toEditText = (EditText) rootView.findViewById(R.id.to_edit_view_id);
 
         return rootView;
 
--- app/src/main/java/technologies/troubleshoot/easytution/TeacherPersonalInfo.java
+++ app/src/main/java/technologies/troubleshoot/easytution/TeacherPersonalInfo.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by kaizer on 2/6/17.
@@ -13,10 +14,25 @@ import android.view.ViewGroup;
 
 public class TeacherPersonalInfo extends Fragment {
 
+    EditText additionalNumberEditText, detailAddressEditText, nidNoEditText, fbIdEditText, linkedId, fatherNameEditText, motherNameEditText, fatherNumberEditText, motherNumberEditText, refPersonNameEditText, contactNumberEditText, relationEditText;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.teacher_personal_info_layout, container, false);
+
+        additionalNumberEditText = (EditText) rootView.findViewById(R.id.additional_number_edit_view_id);
+        detailAddressEditText = (EditText) rootView.findViewById(R.id.detail_address_edit_view_id);
+        nidNoEditText = (EditText) rootView.findViewById(R.id.nid_edit_view_id);
+        fbIdEditText = (EditText) rootView.findViewById(R.id.fb_edit_view_id);
+        linkedId = (EditText) rootView.findViewById(R.id.linked_edit_view_id);
+        fatherNameEditText = (EditText) rootView.findViewById(R.id.father_name_edit_view_id);
+        motherNameEditText = (EditText) rootView.findViewById(R.id.mother_name_edit_view_id);
+        fatherNumberEditText = (EditText) rootView.findViewById(R.id.father_number_edit_view_id);
+        motherNumberEditText = (EditText) rootView.findViewById(R.id.mother_number_edit_view_id);
+        refPersonNameEditText = (EditText) rootView.findViewById(R.id.ref_person_name_edit_view_id);
+        contactNumberEditText = (EditText) rootView.findViewById(R.id.contact_number_edit_view_id);
+        relationEditText = (EditText) rootView.findViewById(R.id.relation_edit_view_id);
 
         return rootView;
 
