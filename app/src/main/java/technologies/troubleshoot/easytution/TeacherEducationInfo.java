debbab5bcea92e9807802d36db7f92e639b2d7f6
kaizer <mir.rayan1@gmail.com>
2017-02-07 14:20:08 +0600

    volly link added to teacher personal and education info

--- app/src/main/java/technologies/troubleshoot/easytution/TeacherEducationInfo.java
+++ app/src/main/java/technologies/troubleshoot/easytution/TeacherEducationInfo.java
@@ -7,6 +7,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by kaizer on 2/6/17.
@@ -14,7 +25,18 @@ import android.widget.EditText;
 
 public class TeacherEducationInfo extends Fragment {
 
-    EditText lastLevelOfStudyEditText, majorEditText, instituteNameEditText, cgpaEditText, yearOfpassingEditText, curriculumEditText, fromEditText, toEditText;
+    private static final String REGISTER_URL = "http://tuition.troubleshoot-tech.com/signup.php";
+    public static final String LAST_LEVEL_OF_STUDY = "last_level_study";
+    public static final String MAJOR = "major_group";
+    public static final String ID_CARD = "id_card";
+    public static final String CGPA = "cgpa";
+    public static final String YEAR_OF_PASSING = "years_of_passing";
+    public static final String CURRICULUM = "curriculam";
+    public static final String FORM_DATE = "year_form";
+    public static final String TO_DATE = "year_to";
+
+
+    EditText lastLevelOfStudyEditText, majorEditText, cgpaEditText, yearOfPassingEditText, curriculumEditText, fromEditText, toEditText;
 
     @Nullable
     @Override
@@ -23,14 +45,53 @@ public class TeacherEducationInfo extends Fragment {
 
         lastLevelOfStudyEditText = (EditText) rootView.findViewById(R.id.last_level_of_study_edit_view_id);
         majorEditText = (EditText) rootView.findViewById(R.id.major_group_edit_view_id);
-        instituteNameEditText = (EditText) rootView.findViewById(R.id.institute_name_edit_view_id);
         cgpaEditText = (EditText) rootView.findViewById(R.id.cgpa_edit_view_id);
-        yearOfpassingEditText = (EditText) rootView.findViewById(R.id.years_of_passing_edit_view_id);
+        yearOfPassingEditText = (EditText) rootView.findViewById(R.id.years_of_passing_edit_view_id);
         curriculumEditText = (EditText) rootView.findViewById(R.id.curriculum_edit_view_id);
         fromEditText = (EditText) rootView.findViewById(R.id.from_edit_view_id);
         toEditText = (EditText) rootView.findViewById(R.id.to_edit_view_id);
 
+        updateEducationInfo(lastLevelOfStudyEditText.getText().toString(), majorEditText.getText().toString(), cgpaEditText.getText().toString(), yearOfPassingEditText.getText().toString(), curriculumEditText.getText().toString(), fromEditText.getText().toString(), toEditText.getText().toString());
+
         return rootView;
 
     }
+
+    private void updateEducationInfo(final String last_level_study, final String major_group, final String cgpa, final String year_of_passing, final String curriculum, final String from_year, final String to_year) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getContext(), "Successfully Registered", Toast.LENGTH_LONG).show();
+                        /*Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                        startActivity(i);*/
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), "No Internet Connection", Toast.LENGTH_LONG).show();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put(LAST_LEVEL_OF_STUDY, last_level_study);
+                params.put(MAJOR, major_group);
+                //params.put(ID_CARD, id_card);
+                params.put(CGPA, cgpa);
+                params.put(YEAR_OF_PASSING, year_of_passing);
+                params.put(CURRICULUM, curriculum);
+                params.put(FORM_DATE, from_year);
+                params.put(TO_DATE, to_year);
+                return params;
+            }
+
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(stringRequest);
+    }
+
 }
--- app/src/main/java/technologies/troubleshoot/easytution/TeacherPersonalInfo.java
+++ app/src/main/java/technologies/troubleshoot/easytution/TeacherPersonalInfo.java
@@ -7,12 +7,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by kaizer on 2/6/17.
  */
 
 public class TeacherPersonalInfo extends Fragment {
+
+    private static final String REGISTER_URL = "http://tuition.troubleshoot-tech.com/signup.php";
+    public static final String ADDITIONAL_NUMBER = "last_level_study";
+    public static final String DETAIL_ADDRESS = "major_group";
+    public static final String LINKED_ID = "id_card";
+    public static final String FATHER_NAME = "cgpa";
+    public static final String MOTHER_NAME = "years_of_passing";
+    public static final String FATHER_NUMBER = "curriculam";
+    public static final String MOTHER_NUMBER = "year_form";
+    public static final String REF_PERSON_NAME = "year_to";
+    public static final String CONTACT_NUMBER = "year_to";
+    public static final String RELATION = "year_to";
 
     EditText additionalNumberEditText, detailAddressEditText, nidNoEditText, fbIdEditText, linkedId, fatherNameEditText, motherNameEditText, fatherNumberEditText, motherNumberEditText, refPersonNameEditText, contactNumberEditText, relationEditText;
 
@@ -37,4 +60,45 @@ public class TeacherPersonalInfo extends Fragment {
         return rootView;
 
     }
+
+    private void updateEducationInfo(final String last_level_study, final String major_group, final String cgpa, final String year_of_passing, final String curriculum, final String from_year, final String to_year, final String id_card) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getContext(), "Successfully Registered", Toast.LENGTH_LONG).show();
+                        /*Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+                        startActivity(i);*/
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), "No Internet Connection", Toast.LENGTH_LONG).show();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put(ADDITIONAL_NUMBER, last_level_study);
+                params.put(DETAIL_ADDRESS, major_group);
+                params.put(LINKED_ID, id_card);
+                params.put(FATHER_NAME, cgpa);
+                params.put(MOTHER_NAME, year_of_passing);
+                params.put(FATHER_NUMBER, curriculum);
+                params.put(MOTHER_NUMBER, from_year);
+                params.put(REF_PERSON_NAME, to_year);
+                params.put(CONTACT_NUMBER, to_year);
+                params.put(RELATION, to_year);
+                return params;
+            }
+
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(stringRequest);
+    }
+
+
 }
